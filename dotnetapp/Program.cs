38a27e7f1a46ed06ee51294ce66d5a5221279f7b
Marco Westerink <m.westerink@rubicon.nl>
2021-09-09 07:26:19 +0200

    cleanup set environment

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,17 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using app.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Sinks.Elasticsearch;
 using Serilog.Sinks.Grafana.Loki;
 
 namespace app
@@ -61,21 +56,11 @@ namespace app
                 .Enrich.WithMachineName()
                 .WriteTo.Debug()
                 .WriteTo.Console()
-                //.WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
-                .WriteTo.GrafanaLoki("http://test-loki.m-westerink-dev.svc.cluster.local:3100", textFormatter: new LokiJsonTextFormatter())
                 .Enrich.WithProperty("Environment", environment)
+                .WriteTo.GrafanaLoki("http://test-loki.m-westerink-dev.svc.cluster.local:3100", textFormatter: new LokiJsonTextFormatter())
                 .ReadFrom.Configuration(configuration)
                 .CreateLogger();
         }
-
-        //private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
-        //{
-        //    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
-        //    {
-        //        AutoRegisterTemplate = true,
-        //        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-        //    };
-        //}
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
